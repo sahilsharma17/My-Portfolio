58e37eadb654a5bf071cd0e8d4bcc16b7d850b82
sagar-7227 <sagarvashnav72427@gmail.com>
2023-09-09 13:45:55 +0530

    add gh-pages

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,98 @@
+/**
+ * Footer component
+ *
+ * Displays avenues to contact you.
+ * Contact information is passed in from the App component that
+ * renders the Footer.
+ *
+ * If a social value has an empty string it will not be displayed.
+ */
+import React from "react";
+import PropTypes from "prop-types";
+
+
+import gitHubIcon from "../images/socials/github.svg";
+import instagramIcon from "../images/socials/instagram.svg";
+import linkedInIcon from "../images/socials/linkedin.svg";
+import twitterIcon from "../images/socials/twitter.svg";
+
+/**
+ * 💡 Learning resources
+ *
+ *  HTML hyperlinks: https://www.w3schools.com/html/html_links.asp
+ *  Opening links in new tabs: https://www.freecodecamp.org/news/how-to-use-html-to-open-link-in-new-tab/
+ */
+
+const Footer = (props) => {
+  const { gitHub, instagram, linkedIn, name, primaryColor, twitter } = props;
+
+  return (
+    <div
+      id="footer"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "2.5rem",
+        padding: "5rem 0 3rem",
+        backgroundColor: primaryColor,
+        width: "100vw"
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "2.5rem",
+        }}
+      >
+        {gitHub && (
+          <a href= "https://github.com/sahilsharma17" target="_blank" rel="noopener noreferrer">
+            <img src={gitHubIcon} alt="GitHub" className="socialIcon" />
+          </a>
+        )}
+        {instagram && (
+          <a
+            href="https://www.instagram.com/sahilshyam17"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={instagramIcon} alt="Instagram" className="socialIcon" />
+          </a>
+        )}
+        {linkedIn && (
+          <a
+            href="https://www.linkedin.com/in/sahil-software-developer/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedInIcon} alt="LinkedIn" className="socialIcon" />
+          </a>
+        )}
+        {twitter && (
+          <a href="https://twitter.com/SahilSh31984303" target="_blank" rel="noopener noreferrer">
+            <img src={twitterIcon} alt="Twitter" className="socialIcon" />
+          </a>
+        )}
+      </div>
+      <p className="small" style={{ marginTop: 0, color: "white" }}>
+        Created by {name}
+      </p>
+    </div>
+  );
+};
+
+Footer.defaultProps = {
+  name: "",
+};
+
+Footer.propTypes = {
+  gitHub: PropTypes.string,
+  instagram: PropTypes.string,
+  linkedIn: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  primaryColor: PropTypes.string,
+  twitter: PropTypes.string,
+};
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/**
+ * Entry point of application, where App is rendered within the div with the id of "app" 
+ */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+import { render } from "react-dom";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import App from "./App";
+
+render(<App></App>, document.getElementById("app"));
